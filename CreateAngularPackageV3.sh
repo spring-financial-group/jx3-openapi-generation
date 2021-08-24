e8fb75bda1c1efa9495cd8984271b081bd4cf580
Paul Craggs <paul@crag.gs>
2021-08-24 10:37:20 +0100

    angular fixes

--- CreateAngularPackageV3.sh
+++ CreateAngularPackageV3.sh
@@ -4,18 +4,24 @@ specurl=$1
 
 echo Spec Url $specurl
 
+cd registry
+
 mkdir -p ./service
 
 case $specurl in
     "http"*) 
 		echo Downloading spec file
-		curl --connect-timeout 30 --retry 300 --retry-delay 5 --retry-connrefused $specurl > ./spec.json	;;
+		curl --connect-timeout 30 --retry 300 --retry-delay 5 --retry-connrefused $specurl > ./spec.json
+		;;
     *) 
 		echo Copying spec file
 		cp $specurl ./spec.json ;;
 esac
 
-npx openapi-generator-cli generate -i ./spec.json -g typescript-angular -o service --additional-properties=fileNaming=camelCase --enable-post-process-file
+
+cp /openapitools.json ./openapitools.json 
+
+npx openapi-generator-cli generate -i ./spec.json -g typescript-angular -o service --additional-properties=fileNaming=camelCase,ngVersion=10.0.0 --enable-post-process-file
 
 cp ./package.json ./service/package.json 
 cp ./tsconfig.json ./service/tsconfig.json 
@@ -24,10 +30,9 @@ cp /configuration.ts ./service/configuration.ts
 cd service
 
 npm install --save rxjs@6.6.7
-npm install --save zone.js@0.9.1
-npm install --save @angular/core@8.2.14 
-npm install --save @angular/common@8.2.14 
-npm install --save typescript
+npm install --save zone.js@0.10.3
+npm install --save @angular/core@10.2.4
+npm install --save @angular/common@10.2.4
 
 ngc 
 
