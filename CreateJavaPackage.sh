27385ed7f0e926f93a1d510ec2cad6e4ff59433b
anton.bulauka <bulavko.anton@mail.ru>
2021-12-08 16:42:16 +0300

    feat: add script for generating java package

---
+++ CreateJavaPackage.sh
@@ -0,0 +1,33 @@
+set -e
+
+specurl=$1
+version=$2
+name=$3
+repoOwner=$4
+repoId=$5
+
+echo Spec Url $specurl
+echo Version $version
+echo Name $name
+
+mkdir -p ./java_service
+
+case $specurl in
+    "http"*)
+		echo Downloading spec file
+		curl -k --http1.1 --connect-timeout 30 --retry 300 --retry-delay 5 --retry-connrefused $specurl > ./spec.json	;;
+    *)
+		echo Copying spec file
+		cp $specurl ./spec.json ;;
+esac
+
+cp /openapitools.json ./openapitools.json
+
+echo Generating API
+npx openapi-generator generate -i ./spec.json -g java --global-property models,modelTests=false,modelDocs=false -p modelPackage=mqube.models -o java_service --git-user-id $repoOwner --git-repo-id $repoId -p dateLibrary=java8 -p java8=true
+
+echo Copying Gradle file
+cp ./build.gradle ./java_service/build.gradle
+
+echo Pushing Package
+gradle publish
