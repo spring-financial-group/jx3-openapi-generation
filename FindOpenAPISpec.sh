fa834a6e53e056f1d26b77d6cfcaa356a4678932
Skisocks <jaa.skeoch@gmail.com>
2022-11-21 11:05:51 +0000

    fix: echo to stderr & use absolute paths

--- FindOpenAPISpec.sh
+++ FindOpenAPISpec.sh
@@ -4,16 +4,16 @@
 # If none is found, return the default path
 DEFAULT_PATH=$1
 
-SWAG_PATH=$(find . -type f -name "swagger.json")
-OPENAPI_PATH=$(find . -type f -name "openapi.json")
+SWAG_PATH=$(find ~+ -type f -name "swagger.json")
+OPENAPI_PATH=$(find ~+ -type f -name "openapi.json")
 if test "$SWAG_PATH" ; then
-  echo "Swagger specification found at $SWAG_PATH"
+  >&2 echo "Swagger specification found at $SWAG_PATH"
   SPEC_PATH=$SWAG_PATH
 elif test "$OPENAPI_PATH" ; then
-  echo "OpenAPI specification found at $OPENAPI_PATH"
+  >&2 echo "OpenAPI specification found at $OPENAPI_PATH"
   SPEC_PATH=$OPENAPI_PATH
 else
-  echo "Specification not found in repository using $DEFAULT_PATH"
+  >&2 echo "Specification not found in repository using $DEFAULT_PATH"
   SPEC_PATH=$DEFAULT_PATH
 fi
 
