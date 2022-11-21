ae1dce8e559e14b57790abc6d284cb1961866916
Archie Skeoch <jaa.skeoch@gmail.com>
2022-11-21 11:19:05 +0000

    refactor: use script to locate openAPI spec and general tidy (#87)
    * refactor: use script to locate openAPI spec and general tidy
    
    * chore: improve naming and logging
    
    * chore: tidying
    
    * fix: echo to stderr & use absolute paths
    
    * fix: path for podIP if cd into ./registry

---
+++ FindOpenAPISpec.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Finds the spec file from the current directory and all subdirectories
+# If none is found, return the default path
+DEFAULT_PATH=$1
+
+SWAG_PATH=$(find ~+ -type f -name "swagger.json")
+OPENAPI_PATH=$(find ~+ -type f -name "openapi.json")
+if test "$SWAG_PATH" ; then
+  >&2 echo "Swagger specification found at $SWAG_PATH"
+  SPEC_PATH=$SWAG_PATH
+elif test "$OPENAPI_PATH" ; then
+  >&2 echo "OpenAPI specification found at $OPENAPI_PATH"
+  SPEC_PATH=$OPENAPI_PATH
+else
+  >&2 echo "Specification not found in repository using $DEFAULT_PATH"
+  SPEC_PATH=$DEFAULT_PATH
+fi
+
+echo "$SPEC_PATH"
