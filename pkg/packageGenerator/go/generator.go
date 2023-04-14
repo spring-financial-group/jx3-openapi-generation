b6481dcd623c6f9fc309a260bdbd8b6300c2b973
Skisocks <jaa.skeoch@gmail.com>
2023-04-14 22:24:23 +0100

    chore: general things to get it working

--- pkg/packageGenerator/go/generator.go
+++ pkg/packageGenerator/go/generator.go
@@ -6,6 +6,7 @@ import (
 	gh "github.com/google/go-github/v47/github"
 	"github.com/pkg/errors"
 	"path/filepath"
+	"regexp"
 	"spring-financial-group/jx3-openapi-generation/pkg/domain"
 	"spring-financial-group/jx3-openapi-generation/pkg/git"
 	"spring-financial-group/jx3-openapi-generation/pkg/packageGenerator"
@@ -17,8 +18,6 @@ import (
 const (
 	PushRepositoryURL  = "https://github.com/spring-financial-group/mqube-go-packages.git"
 	PushRepositoryName = "mqube-go-packages"
-
-	updateBotLabel = "updatebot"
 )
 
 var (
@@ -51,9 +50,29 @@ func (g *Generator) GeneratePackage(outputDir string) (string, error) {
 		return "", errors.Wrap(err, "failed to checkout branch")
 	}
 
+	g.setDynamicConfigVariables()
+
 	packageDir, err := g.BaseGenerator.GeneratePackage(filepath.Join(repoDir, g.GetPackageName()), domain.Go)
 	if err != nil {
 		return "", err
+	}
+
+	// Openapitools sets the module name to the REPO_NAME, we need it to be the PushRepositoryName
+	err = g.changeModuleName(packageDir)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to change module name")
+	}
+
+	// Run go mod tidy to ensure the go.mod file doesn't have any unnecessary dependencies
+	err = g.goModTidy(packageDir)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to run go mod tidy")
+	}
+
+	// We need to be able to identify the version of the package from within the repository
+	err = g.createPackageVersionFile(packageDir)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to create package version file")
 	}
 
 	err = g.Git.AddFiles(repoDir, packageDir)
@@ -61,7 +80,7 @@ func (g *Generator) GeneratePackage(outputDir string) (string, error) {
 		return "", errors.Wrap(err, "failed to add files to Git")
 	}
 
-	err = g.Git.Commit(repoDir, fmt.Sprintf("chore(deps): upgrade %s package -> %s", g.GetPackageName(), g.Version))
+	err = g.Git.Commit(repoDir, fmt.Sprintf("chore(deps): upgrade %s module -> %s", g.GetPackageName(), g.Version))
 	if err != nil {
 		return "", errors.Wrap(err, "failed to commit package")
 	}
@@ -73,7 +92,38 @@ func (g *Generator) GetPackageName() string {
 }
 
 func (g *Generator) setDynamicConfigVariables() {
-	g.Cfg.GeneratorCLI.Generators[domain.Python].AdditionalProperties["packageName"] = g.GetPackageName()
+	g.Cfg.GeneratorCLI.Generators[domain.Go].AdditionalProperties["packageName"] = g.GetPackageName()
+}
+
+func (g *Generator) changeModuleName(packageDir string) error {
+	goModPath := filepath.Join(packageDir, "go.mod")
+	bytes, err := g.FileIO.Read(goModPath)
+	if err != nil {
+		return errors.Wrap(err, "failed to read file")
+	}
+
+	newModuleName := fmt.Sprintf("github.com/spring-financial-group/%s/%s", PushRepositoryName, g.GetPackageName())
+
+	re, err := regexp.Compile(`module .*`)
+	if err != nil {
+		return errors.Wrap(err, "failed to compile regex")
+	}
+
+	bytes = re.ReplaceAll(bytes, []byte(fmt.Sprintf("module %s", newModuleName)))
+
+	err = g.FileIO.Write(goModPath, bytes, 0700)
+	if err != nil {
+		return errors.Wrap(err, "failed to write file")
+	}
+	return nil
+}
+
+func (g *Generator) goModTidy(dir string) error {
+	return g.Cmd.ExecuteAndLog(dir, "go", "mod", "tidy")
+}
+
+func (g *Generator) createPackageVersionFile(packageDir string) error {
+	return g.FileIO.Write(filepath.Join(packageDir, "VERSION"), []byte(g.Version), 0700)
 }
 
 func (g *Generator) PushPackage(packageDir string) error {
@@ -119,9 +169,5 @@ func (g *Generator) createPullRequest(currentBranch, defaultBranch string) error
 	if err != nil {
 		return errors.Wrap(err, "failed to add reviewers to pull request")
 	}
-	//_, err = g.Scm.AddLabels(context.Background(), []string{updateBotLabel}, pr.GetNumber())
-	//if err != nil {
-	//	return errors.Wrap(err, "failed to add labels pull request")
-	//}
 	return nil
 }
