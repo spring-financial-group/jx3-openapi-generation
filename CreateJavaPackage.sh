445f95a25de2adc8a5292db168874a70a45272db
luisjones <11049493+luisjones@users.noreply.github.com>
2024-08-11 21:24:25 +0100

    fix: specify a library

--- CreateJavaPackage.sh
+++ CreateJavaPackage.sh
@@ -24,7 +24,7 @@ esac
 cp /openapitools.json ./openapitools.json
 
 echo Generating API
-npx openapi-generator-cli generate -i ./spec.json -g java -o java_service --git-user-id $repoOwner --git-repo-id $repoId --global-property models,modelTests=false,modelDocs=false -p basePackage=$name -p modelPackage=${name}.models -p dateLibrary=java8-localdatetime -p serializationLibrary=jackson
+npx openapi-generator-cli generate -i ./spec.json -g java -o java_service --git-user-id $repoOwner --git-repo-id $repoId --global-property models,modelTests=false,modelDocs=false -p basePackage=$name -p modelPackage=${name}.models -p dateLibrary=java8-localdatetime -p serializationLibrary=jackson -p library=restclient
 
 echo "Set version to build.gradle" && sed -i "s|version = '0.0.0'|version = '${version}'|" ./build.gradle
 echo "Copying Gradle file" && cp ./build.gradle ./java_service/build.gradle
