50bd743b6d143a3df0079fc38861cc63a2ed2ff9
William Neild <wneild5@gmail.com>
2025-09-02 11:32:59 +0100

    feat: rust git-based package generation

--- pkg/packageGenerator/rust/generator.go
+++ pkg/packageGenerator/rust/generator.go
@@ -1,37 +1,80 @@
 package rust
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"path/filepath"
+	"strings"
 
+	gh "github.com/google/go-github/v47/github"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"github.com/spring-financial-group/jx3-openapi-generation/pkg/domain"
+	"github.com/spring-financial-group/jx3-openapi-generation/pkg/git"
 	"github.com/spring-financial-group/jx3-openapi-generation/pkg/packageGenerator"
-	"github.com/spring-financial-group/mqa-logging/pkg/log"
+	"github.com/spring-financial-group/jx3-openapi-generation/pkg/scmClient/github"
+	"github.com/spring-financial-group/jx3-openapi-generation/pkg/utils"
 )
 
 const (
-	packagingFilesDir = "/templates/rust"
+	PushRepositoryURL  = "https://github.com/spring-financial-group/mqube-rust-packages.git"
+	PushRepositoryName = "mqube-rust-packages"
+	updateBotLabel     = "updatebot"
 )
 
 type Generator struct {
 	*packageGenerator.BaseGenerator
+	Git domain.Gitter
+	Scm domain.ScmClient
 }
 
 func NewGenerator(baseGenerator *packageGenerator.BaseGenerator) *Generator {
 	return &Generator{
 		BaseGenerator: baseGenerator,
+		Git:           git.NewClient(),
+		Scm:           github.NewClient(baseGenerator.RepoOwner, PushRepositoryName, baseGenerator.GitToken),
 	}
 }
 
 func (g *Generator) GeneratePackage(outputDir string) (string, error) {
 	g.setDynamicConfigVariables()
 
-	packageDir, err := g.BaseGenerator.GeneratePackage(filepath.Join(outputDir, g.GetPackageName()), domain.Rust)
+	repoDir, err := g.Git.Clone(outputDir, PushRepositoryURL)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to clone packages repository")
+	}
+
+	branchName := fmt.Sprintf("update/%s/%s", g.GetPackageName(), g.Version)
+	err = g.Git.CheckoutBranch(repoDir, branchName)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to checkout branch")
+	}
+
+	packageDir := filepath.Join(repoDir, g.GetPackageName())
+	err = g.createFreshDir(packageDir)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to create fresh package dir")
+	}
+
+	_, err = g.BaseGenerator.GeneratePackage(packageDir, domain.Rust)
 	if err != nil {
 		return "", err
 	}
 
-	if err = g.FileIO.TemplateFilesInDir(packagingFilesDir, packageDir, g); err != nil {
-		return "", err
+	err = g.FileIO.Write(filepath.Join(packageDir, "VERSION"), []byte(g.Version), 0700)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to write VERSION file")
+	}
+
+	err = g.Git.AddFiles(repoDir, packageDir)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to add files to Git")
+	}
+
+	err = g.Git.Commit(repoDir, fmt.Sprintf("chore(deps): upgrade %s module -> %s", g.GetPackageName(), g.Version))
+	if err != nil {
+		return "", errors.Wrap(err, "failed to commit package")
 	}
 
 	return packageDir, nil
@@ -47,7 +90,66 @@ func (g *Generator) GetPackageName() string {
 }
 
 func (g *Generator) PushPackage(packageDir string) error {
-	out, err := g.Cmd.Execute(packageDir, "cargo", "publish", "--no-verify", "--allow-dirty")
-	log.Logger().Info(out)
-	return err
+	currentBranch, err := g.Git.GetCurrentBranch(packageDir)
+	if err != nil {
+		return errors.Wrap(err, "failed to get current branch")
+	}
+
+	err = g.Git.Push(packageDir, currentBranch)
+	if err != nil {
+		return errors.Wrap(err, "failed to Git push package")
+	}
+
+	defaultBranch, err := g.Git.GetDefaultBranchName(packageDir)
+	if err != nil {
+		return errors.Wrap(err, "failed to get default branch name")
+	}
+
+	err = g.createPullRequest(currentBranch, defaultBranch)
+	if err != nil {
+		return errors.Wrap(err, "failed to create pull request")
+	}
+	return nil
+}
+
+func (g *Generator) createFreshDir(packageDir string) error {
+	// Check if directory exists
+	if _, err := os.Stat(packageDir); err == nil {
+		// Remove entire directory and its contents
+		if err := os.RemoveAll(packageDir); err != nil {
+			return errors.Wrapf(err, "failed to remove existing directory: %s", packageDir)
+		}
+		logrus.Infof("Removed existing directory: %s", packageDir)
+	}
+
+	// Create a fresh directory
+	if err := os.MkdirAll(packageDir, 0755); err != nil {
+		return errors.Wrapf(err, "failed to create directory: %s", packageDir)
+	}
+	fmt.Println("Created directory:", packageDir)
+
+	return nil
+}
+
+func (g *Generator) createPullRequest(currentBranch, defaultBranch string) error {
+	pr, err := g.Scm.CreatePullRequest(
+		context.Background(),
+		&gh.NewPullRequest{
+			Title:               utils.NewPtr(fmt.Sprintf("chore(deps): upgrade %s package -> %s", g.GetPackageName(), g.Version)),
+			Head:                &currentBranch,
+			Base:                utils.NewPtr(strings.TrimPrefix(defaultBranch, "origin/")),
+			Body:                utils.NewPtr(fmt.Sprintf("Automated rust package update for %s", g.GetPackageName())),
+			MaintainerCanModify: utils.NewPtr(true),
+		},
+	)
+	if err != nil {
+		return errors.Wrap(err, "failed to create pull request")
+	}
+
+	// auto-merge labels
+	_, err = g.Scm.AddLabels(context.Background(), []string{updateBotLabel}, pr.GetNumber())
+	if err != nil {
+		return errors.Wrap(err, "failed to add labels pull request")
+	}
+	return nil
 }
