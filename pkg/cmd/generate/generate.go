9670d2ba2805497d9436b70831fb802e8aad62ea
wsngamerz <wneild5@gmail.com>
2026-07-07 09:40:14 +0100

    feat: reduce required args if not pushing

--- pkg/cmd/generate/generate.go
+++ pkg/cmd/generate/generate.go
@@ -87,12 +87,22 @@ func NewCmdGenerate() *cobra.Command {
 		},
 		SuggestFor: []string{"genarate, genorate"},
 		Aliases:    []string{"gen"},
-		// Initialize environment variables at execution time, not creation time
-		// Use PersistentPreRunE so it runs before subcommands' PreRunE
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			return o.initialise()
+			o.Cmd = cmd
+			return o.validateOptions()
 		},
 	}
+
+	cmd.PersistentFlags().StringVar(&o.Version, "version", os.Getenv(versionKey), fmt.Sprintf("Package version (env %s)", versionKey))
+	cmd.PersistentFlags().StringVar(&o.RepoOwner, "repo-owner", os.Getenv(repoOwnerKey), fmt.Sprintf("Repository owner (env %s)", repoOwnerKey))
+	cmd.PersistentFlags().StringVar(&o.RepoName, "repo-name", os.Getenv(repoNameKey), fmt.Sprintf("Repository name (env %s)", repoNameKey))
+	cmd.PersistentFlags().StringVar(&o.SwaggerServiceName, "swagger-service-name", os.Getenv(swaggerServiceNameKey), fmt.Sprintf("Service name for package generation (env %s)", swaggerServiceNameKey))
+	cmd.PersistentFlags().StringVar(&o.ServerVariables, "server-variables", os.Getenv(serverVariables), fmt.Sprintf("Server variables (env %s)", serverVariables))
+	cmd.PersistentFlags().StringVar(&o.SpecPath, "spec-path", os.Getenv(specPathKey), fmt.Sprintf("Path to OpenAPI spec file (env %s)", specPathKey))
+	cmd.PersistentFlags().StringVar(&o.GitUser, "git-user", os.Getenv(gitUserKey), fmt.Sprintf("Git username (env %s)", gitUserKey))
+	cmd.PersistentFlags().StringVar(&o.GitToken, "git-token", os.Getenv(gitTokenKey), fmt.Sprintf("Git authentication token (env %s)", gitTokenKey))
+	cmd.PersistentFlags().StringVar(&o.PackageName, "package-name", utils.FirstNonEmpty(os.Getenv(packageNameKey), "Client"), fmt.Sprintf("Package name (env %s, defaults to \"Client\")", packageNameKey))
+	cmd.PersistentFlags().BoolVar(&o.SkipPush, "skip-push", os.Getenv(skipPushKey) == "true", fmt.Sprintf("Skip pushing generated packages (env %s)", skipPushKey))
 
 	cmd.AddCommand(NewCmdGeneratePackages(o))
 	return cmd
@@ -103,49 +113,38 @@ func (o *Options) Run() error {
 	return o.Cmd.Help()
 }
 
-func (o *Options) initialise() error {
-	err := o.getVariablesFromEnvironment()
-	if err != nil {
+func (o *Options) validateOptions() error {
+	if err := o.validateRequiredOptions(); err != nil {
 		return err
 	}
-	err = o.validateSpecificationLocation()
-	if err != nil {
-		return err
-	}
-	return nil
+	return o.validateSpecificationLocation()
 }
 
-func (o *Options) getVariablesFromEnvironment() error {
+// validateRequiredOptions checks that options backed by a required flag/env var have been
+// resolved to a non-empty value, whether set via CLI flag or the flag's env-derived default.
+func (o *Options) validateRequiredOptions() error {
 	var missingVariables []string
-	if o.Version = os.Getenv(versionKey); o.Version == "" {
-		missingVariables = append(missingVariables, versionKey)
+
+	required := map[string]string{
+		swaggerServiceNameKey: o.SwaggerServiceName,
+		specPathKey:           o.SpecPath,
 	}
-	if o.RepoOwner = os.Getenv(repoOwnerKey); o.RepoOwner == "" {
-		missingVariables = append(missingVariables, repoOwnerKey)
+
+	// we only require git credentials and repo info if we're not skipping the push step
+	if !o.SkipPush {
+		required[gitUserKey] = o.GitUser
+		required[gitTokenKey] = o.GitToken
+		required[versionKey] = o.Version
+		required[repoOwnerKey] = o.RepoOwner
+		required[repoNameKey] = o.RepoName
 	}
-	if o.RepoName = os.Getenv(repoNameKey); o.RepoName == "" {
-		missingVariables = append(missingVariables, repoNameKey)
+
+	for envKey, value := range required {
+		if value == "" {
+			missingVariables = append(missingVariables, envKey)
+		}
 	}
-	if o.SwaggerServiceName = os.Getenv(swaggerServiceNameKey); o.SwaggerServiceName == "" {
-		missingVariables = append(missingVariables, swaggerServiceNameKey)
-	}
-	if o.PackageName = os.Getenv(packageNameKey); o.PackageName == "" {
-		o.PackageName = "Client"
-	}
-	if o.SpecPath = os.Getenv(specPathKey); o.SpecPath == "" {
-		missingVariables = append(missingVariables, specPathKey)
-	}
-	if o.GitUser = os.Getenv(gitUserKey); o.GitUser == "" {
-		missingVariables = append(missingVariables, gitUserKey)
-	}
-	if o.GitToken = os.Getenv(gitTokenKey); o.GitToken == "" {
-		missingVariables = append(missingVariables, gitTokenKey)
-	}
-	o.ServerVariables = os.Getenv(serverVariables)
-	// Check if SKIP_PUSH is set to "true"
-	if skipPush := os.Getenv(skipPushKey); skipPush == "true" {
-		o.SkipPush = true
-	}
+
 	if len(missingVariables) > 0 {
 		return &domain.EnvironmentVariableNotFoundError{VariableNames: missingVariables}
 	}
