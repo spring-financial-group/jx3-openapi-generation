29a4110f61d8248da919af2dc0b96f4fe51680a8
wsngamerz <wneild5@gmail.com>
2026-07-08 08:49:36 +0100

    fix: invalid package name handling in git add for go and rust

--- pkg/packagegenerator/go/generator.go
+++ pkg/packagegenerator/go/generator.go
@@ -105,7 +105,7 @@ func (g *Generator) GeneratePackage(outputDir string) (string, error) {
 		return "", errors.Wrap(err, "failed to create package version file")
 	}
 
-	err = g.Git.AddFiles(repoDir, packageDir)
+	err = g.Git.AddFiles(repoDir, g.GetPackageName())
 	if err != nil {
 		return "", errors.Wrap(err, "failed to add files to Git")
 	}
